cd3e7d5c08edbdb678335825bfbfbf54babbec15
LastSkywalkerER <maxdr1998@gmail.com>
2021-01-28 21:50:17 +0300

    lesson1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let money = 0;
+let income = 0;
+let addExpenses = 0;
+let deposit = 0;
+let mission = 0;
+let period = 0;
+
+alert('Вывести на экран в модальном окне (alert) сообщение с любым текстом');
+console.log('Вывести в консоль сообщение с любым текстом');
