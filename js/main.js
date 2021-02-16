af1801025ff9145f24e2badc1ab15c894348e960
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-16 17:28:15 +0300

    lesson14 hard

--- js/main.js
+++ js/main.js
@@ -1,41 +1,79 @@
-'use strict';
+document.addEventListener("DOMContentLoaded", () => {
 
-function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-}
+  'use strict';
 
-DomElement.prototype.createElement = function () {
-  let element = '';
-
-  if (this.selector[0] === '.') {
-    element = document.createElement('div');
-    element.classList.add(this.selector.slice(1));
+  function DomElement(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.position = 'absolute';
+    this.positionX = 0;
+    this.positionY = 0;
+    this.element = '';
   }
 
-  if (this.selector[0] === '#') {
-    element = document.createElement('p');
-    element.id = this.selector.slice(1);
-  }
+  DomElement.prototype.create = function () {
 
-  element.style.height = this.height;
-  element.style.width = this.width;
-  element.style.background = this.bg;
-  element.style.fontSize = this.fontSize;
+    if (this.selector[0] === '.') {
+      this.element = document.createElement('div');
+      this.element.classList.add(this.selector.slice(1));
+    }
 
-  element.textContent = 'Hello World!';
-  element.style.display = 'flex';
-  element.style.alignItems = 'center';
-  element.style.justifyContent = 'center';
+    if (this.selector[0] === '#') {
+      this.element = document.createElement('p');
+      this.element.id = this.selector.slice(1);
+    }
 
-  document.querySelector('body').append(element);
-};
+    this.element.style.height = this.height;
+    this.element.style.width = this.width;
+    this.element.style.background = this.bg;
+    this.element.style.fontSize = this.fontSize;
 
-let createDiv = new DomElement('.magic', '100px', '100px', 'red', '15px');
-createDiv.createElement();
+    this.element.style.position = this.position;
+    this.element.style.left = this.positionX + 'px';
+    this.element.style.top = this.positionY + 'px';
 
-let createP = new DomElement('#magic', '150px', '150px', 'pink', '20px');
-createP.createElement();
+    this.element.textContent = 'Hello World!';
+    this.element.style.display = 'flex';
+    this.element.style.alignItems = 'center';
+    this.element.style.justifyContent = 'center';
+
+    document.addEventListener('keydown', this.move.bind(this));
+
+    document.querySelector('body').append(this.element);
+
+  };
+
+  DomElement.prototype.reDrow = function () {
+    this.element.remove();
+    this.element.style.left = this.positionX + 'px';
+    this.element.style.top = this.positionY + 'px';
+    document.querySelector('body').append(this.element);
+  };
+
+  DomElement.prototype.move = function (event) {
+    switch (event.key) {
+      case 'ArrowRight':
+        this.positionX += 10;
+        break;
+      case 'ArrowUp':
+        this.positionY -= 10;
+        break;
+      case 'ArrowDown':
+        this.positionY += 10;
+        break;
+      case 'ArrowLeft':
+        this.positionX -= 10;
+        break;
+    }
+    this.reDrow();
+  };
+
+  let createDiv = new DomElement('.magic', '100px', '100px', 'red', '15px');
+  createDiv.create();
+
+
+
+});
